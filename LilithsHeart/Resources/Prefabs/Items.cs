10d895657d52f40187579a4398595ed43d489d53
AudaciousBovine <audaciousbovine@gmail.com>
2026-05-16 15:45:00 -0700

    Add PrefabGUID resource system with Items and prefab-item-names config

---
+++ LilithsHeart/Resources/Prefabs/Items.cs
@@ -0,0 +1,14 @@
+using Stunlock.Core;
+
+namespace LilithsHeart.Resources.Prefabs;
+
+public static class Items
+{
+    // Wood
+    public static readonly PrefabGUID Item_Ingredient_Wood_Standard = new(-1593377811);
+    public static readonly PrefabGUID Item_Ingredient_Plank = new(-1017402979);
+    
+    // Stone
+    public static readonly PrefabGUID Item_Ingredient_Stone = new(-1531666018);
+    public static readonly PrefabGUID Item_Ingredient_StoneBrick = new(1788016417);
+}
